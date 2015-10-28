d0b53492bdd98ee7e4e8b5aa6771bf1a186e52f9
Owen Smith <owen@omsmith.ca>
2015-10-27 22:01:07 -0400

    chore(style) comply with eslint-brightspace

--- spec/basic.js
+++ spec/basic.js
@@ -4,18 +4,17 @@ var expect = require('chai').expect;
 
 var framed = require('../');
 
-describe('framed', function () {
-
-	beforeEach(function () {
+describe('framed', function() {
+	beforeEach(function() {
 		global.window = {};
 	});
 
-	it('should return true if D2L is undefined', function () {
+	it('should return true if D2L is undefined', function() {
 		var val = framed();
 		expect(val).to.be.true;
 	});
 
-	it('should return false if D2L is defined', function () {
+	it('should return false if D2L is defined', function() {
 		global.window.D2L = {};
 
 		var val = framed();
