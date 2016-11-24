9f0af157e35b9aee8c434823cdcdd8afe6a4ba1d
Hekar Khani <Hekar.Khani@D2L.com>
2016-11-24 17:27:38 -0500

    Allow client to override `framed()` check
    Override the framed check by setting window.D2L.frau.forceFramed to true
    
    For example:
    ```
    Object.assign(window.D2L.frau, { forceFramed: true });
    ```
    
    This was change was made, because Polymer Free-range applications
    located in an iframe using Brightspace components
    have `window.D2L` defined.

--- src/framed.js
+++ src/framed.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function forceFramed(d2l) {
+	return !!(d2l && d2l.frau && d2l.frau.forceFramed);
+}
+
 module.exports = function framed() {
-	return !window.D2L;
+	return !window.D2L || forceFramed(window.D2L);
 };
